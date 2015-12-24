0011565d4e09b2603e0d64c20100fbcd0c65a203
Josh Montague <joshua.montague@gmail.com>
2015-12-24 21:26:19 +0000

    Add flags for test/predict and server/not in lieu of commenting

--- launch-processes.bash
+++ launch-processes.bash
@@ -10,26 +10,41 @@ PY=${VENV}/bin/python
 # manual work-around for ubuntu
 PY=python
 
+
+####################  config  #############################
 # range of experiments to run 
-#SEQUENCE=`seq 27 29`
-SEQUENCE=32
+SEQUENCE=`seq 33 34`
+#SEQUENCE=32
 
-# experiments or submissions?
-#SCRIPT=run-experiment.py
-SCRIPT=full-train-and-predict.py
+# are we doing a cv split ("experiment") or full test prediction?
+EXPERIMENT=true 
 
-# for experiments
-NICE="nice -n15"
-# for submissions
-#NICE="nice"
-
-# experiments (+plotting)
-ARGS='--verbose --ubuntu'
-# submission
-#ARGS='--verbose'
+# server or laptop?
+SERVER=true
 
 # stagger the workload 
-SLEEPTIME=300
+SLEEPTIME=60
+###########################################################
+
+
+# remember bash boolean conditionals are confusing; just 
+#   use convenient strings
+# http://stackoverflow.com/a/21210966/1851811
+if [ "${EXPERIMENT}" = true ]; then
+    SCRIPT=run-experiment.py
+else
+    SCRIPT=full-train-and-predict.py
+fi
+
+if [ ${SERVER} = "true" ]; then
+    ARGS='--verbose --ubuntu'
+else
+    ARGS='--verbose'
+fi
+
+
+# be polite 
+NICE="nice -n15"
 
 
 echo "$(date +%Y-%m-%d\ %H:%M:%S) -- started running $0"
