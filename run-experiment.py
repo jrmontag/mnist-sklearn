9b0db1e8452ceb35606d36ff4794d8f3e271a962
Josh Montague <joshua.montague@gmail.com>
2015-12-28 04:12:19 +0000

    Handle joblib error so we dont crash

--- run-experiment.py
+++ run-experiment.py
@@ -86,7 +86,12 @@ model_name = utils.short_name(pipeline) + \
                 '_cv-split_' + \
                 datetime.utcnow().strftime('%Y-%m-%d_%H%M%S') 
 logging.info('writing fit pipeline to disk as {}'.format(model_name)) 
-joblib.dump(pipeline, os.path.join('saved_models', model_name) + '.pkl', compress=3)
+try:
+    joblib.dump(pipeline, os.path.join('saved_models', model_name) + '.pkl', compress=3)
+except OverflowError, e:
+    # this is annoying; look deeper, later
+    logging.warn('joblib write failed with error={}'.format(e)) 
+    logging.info('proceeding with predictions without writing model to disk')
 
 # if we used a gridsearch, this is simple 
 if hasattr(pipeline, 'best_params_'):
