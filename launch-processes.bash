b46764d2910fab8b63c44d33b3184900a612940c
Josh Montague <joshua.montague@gmail.com>
2015-12-29 22:31:59 +0000

    Improve commenting in launch-process.bash

--- launch-processes.bash
+++ launch-processes.bash
@@ -19,8 +19,8 @@ source /home/jmontague/CCC-venv/bin/activate
 
 ####################  config  #############################
 # range of experiments to run 
-#SEQUENCE=`seq 45 46`
-SEQUENCE=52
+#SEQUENCE=`seq 28 29`
+SEQUENCE=42
 
 # are we doing a cv split ("experiment") or full test prediction?
 EXPERIMENT=false
@@ -28,11 +28,15 @@ EXPERIMENT=false
 # server or laptop?
 SERVER=true
 
+# expanded (or original) data?
+EXPANDED=true
+
+
 # stagger the workload 
 # 300 = 5 min
 # 600 = 10 min
 # 1200 = 20 min
-SLEEPTIME=300
+SLEEPTIME=900
 ###########################################################
 
 
@@ -46,26 +50,31 @@ if [ "${EXPERIMENT}" = true ]; then
 else
     SCRIPT=full-train-and-predict.py
     # be slightly less polite 
-    NICE="nice -n10"
+    NICE="nice -n5"
 fi
 
 if [ "${SERVER}" = true ]; then
-    ARGS='--verbose --ubuntu'
+    ARGS="--verbose --ubuntu"
 else
-    ARGS='--verbose'
+    ARGS="--verbose"
+fi
+
+if [ "${EXPANDED}" = true ]; then
+    ARGS="${ARGS} --expanded"
 fi
 
 
-
 echo "$(date +%Y-%m-%d\ %H:%M:%S) -- started running $0"
-
-filedate="$(date +%Y-%m-%dT%H:%M:%S)"
-
 echo "$(date +%Y-%m-%d\ %H:%M:%S) -- using python interpreter: $(which python)"
 
+# name all of these processes similarly 
+filedate="$(date +%Y-%m-%dT%H:%M:%S)"
+
 for i in ${SEQUENCE}; do
-    echo "$(date +%Y-%m-%d\ %H:%M:%S) -- launching experiment ${i}"
+    echo "$(date +%Y-%m-%d\ %H:%M:%S) -- launching experiment ${i} with ${SCRIPT} and ARGS=${ARGS}"
+    # launch the appropriate process 
     nohup ${NICE} ${PY} ${SCRIPT} expt_${i} ${ARGS} > log/${filedate}_expt_${i}.log & 
+    # note this will also sleep after the last process
     echo "$(date +%Y-%m-%d\ %H:%M:%S) -- sleeping for ${SLEEPTIME} seconds"
     sleep ${SLEEPTIME} 
 done
@@ -75,5 +84,4 @@ echo "$(date +%Y-%m-%d\ %H:%M:%S) -- deactivating virtualenv"
 deactivate
 
 echo "$(date +%Y-%m-%d\ %H:%M:%S) -- interpreter now set to: $(which python)"
-
 echo "$(date +%Y-%m-%d\ %H:%M:%S) -- finished launching experiments"
