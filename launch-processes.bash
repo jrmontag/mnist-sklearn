ff4bbe9bd525668bcbbf3e365990a5ea3845e735
Josh Montague <joshua.montague@gmail.com>
2016-01-09 02:52:50 +0000

    Pare to single python script, consolidate CLI args

--- launch-processes.bash
+++ launch-processes.bash
@@ -4,59 +4,60 @@
 # This script is used to launch a set of experiments or processes 
 #
 
-# vars
-#VENV=tmp-venv
-#PY=${VENV}/bin/python
-
-# manual work-around for ubuntu system install
-PY=python
-# necessary once we include MLP - dev release of sklearn in virtual env
-source /home/jmontague/CCC-venv/bin/activate
-# I think we don't need this anymore b/c of symlink
-#export PYTHONPATH=~/CCC-venv/lib/python2.7/site-packages/scikit-learn:$PYTHONPATH
-
-
-
 ####################  config  #############################
 # range of experiments to run 
-#SEQUENCE=`seq 28 29`
-SEQUENCE=42
+SEQUENCE=`seq 1 4`
+#SEQUENCE=42
 
-# are we doing a cv split ("experiment") or full test prediction?
-EXPERIMENT=false
+# are we doing a cv split ("experiment")? (vs. train and predict on test) 
+EXPERIMENT=true
 
-# server or laptop?
+# server (ubuntu)? (vs. laptop)
 SERVER=true
 
-# expanded (or original) data?
-EXPANDED=true
+# expanded dataset? (vs. as-given)
+EXPANDED=false
 
-
-# stagger the workload 
+# stagger the workload (seconds)
+SLEEPTIME=60
+# 60 = 1 min
 # 300 = 5 min
 # 600 = 10 min
 # 1200 = 20 min
-SLEEPTIME=900
 ###########################################################
+
+# variables
+VENV=tmp-venv
+
+# Python script
+PY_SCRIPT=build-model.py
+
+# necessary once we include MLP - dev release of sklearn in virtual env
+#source /home/jmontague/CCC-venv/bin/activate
+# - replace ^ with v for testing makefile
+source ${VENV}/bin/activate 
+
+# set some defaults
+ARGS="--verbose"
+NICE="nice -n10"
 
 
 # remember bash boolean conditionals are confusing; just 
 #   use convenient strings
 # http://stackoverflow.com/a/21210966/1851811
 if [ "${EXPERIMENT}" = true ]; then
-    SCRIPT=run-experiment.py
+    #SCRIPT=run-experiment.py
     # be polite 
     NICE="nice -n15"
 else
-    SCRIPT=full-train-and-predict.py
+    #SCRIPT=full-train-and-predict.py
     # be slightly less polite 
     NICE="nice -n5"
+    ARGS="${ARGS} --submission"
 fi
 
 if [ "${SERVER}" = true ]; then
-    ARGS="--verbose --ubuntu"
-else
-    ARGS="--verbose"
+    ARGS="${ARGS} --ubuntu"
 fi
 
 if [ "${EXPANDED}" = true ]; then
@@ -73,7 +74,8 @@ filedate="$(date +%Y-%m-%dT%H:%M:%S)"
 for i in ${SEQUENCE}; do
     echo "$(date +%Y-%m-%d\ %H:%M:%S) -- launching experiment ${i} with ${SCRIPT} and ARGS=${ARGS}"
     # launch the appropriate process 
-    nohup ${NICE} ${PY} ${SCRIPT} expt_${i} ${ARGS} > log/${filedate}_expt_${i}.log & 
+    #nohup ${NICE} ${PY} ${SCRIPT} expt_${i} ${ARGS} > log/${filedate}_expt_${i}.log & 
+    nohup ${NICE} python ${PY_SCRIPT} expt_${i} ${ARGS} > log/${filedate}_expt_${i}.log & 
     # note this will also sleep after the last process
     echo "$(date +%Y-%m-%d\ %H:%M:%S) -- sleeping for ${SLEEPTIME} seconds"
     sleep ${SLEEPTIME} 
